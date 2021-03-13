1543a5ae9fd17e85998bac2c36c45820c2ff20c5
Jeorge Donato <jeorgekhenr@gmail.com>
2021-03-13 13:19:19 -0800

    Added api

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import apiSearch from './api'
+const { useState, useEffect } = React
 export default function App () {
+  const [movieInfo, setMovieInfo] = useState('')
+
+  useEffect(() => {
+    setMovieInfo(JSON.stringify(apiSearch('matrix')))
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{movieInfo}</Text>
       <StatusBar style='auto' />
     </View>
   )
---
+++ api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const APIKEY = 'trilogy'
+const searchApi = async (title, plot = null, year = null) => {
+  try {
+    const apiStr = `http://www.omdbapi.com/?apikey=${APIKEY}?t=${title}${year ? '&y=' + year : ''}${plot ? '&plot=' + plot : ''}`
+    console.log(apiStr)
+    return await axios.get(apiStr)
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export default searchApi
